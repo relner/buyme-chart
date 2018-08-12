408b9c46239492e890eed3f404f49f59bcd0864f
“relner” <vladimir@relner.com>
2018-08-13 01:17:46 +0300

    Resize and move nlock

--- src/app/directives/resizer.directive.ts
+++ src/app/directives/resizer.directive.ts
@@ -7,57 +7,86 @@ export class ResizerDirective {
 
   constructor(private elementRef: ElementRef) { }
 
-  charSize = { width: 400, height: 400}
+  charSize = { width: 400, height: 400, left: 0, top: 0}
 
   ngOnInit() {
     this.setSizeToElement();
     
-    let resizingWrap = this.cretaResizingWrapp();
-    this.resizer(resizingWrap);
+    let createMoverAndResizer = this.cretaResizingWrapp()
+    this.resizer(createMoverAndResizer);
   }
 
   setSizeToElement(){
     this.charSize = JSON.parse(localStorage.getItem('charSize')) || this.charSize;
-    this.elementRef.nativeElement.style.position = 'relative'    
+    this.elementRef.nativeElement.style.position = 'fixed'    
     this.elementRef.nativeElement.style.width = this.charSize.width + 'px';
     this.elementRef.nativeElement.style.height = this.charSize.height + 'px';
+    this.elementRef.nativeElement.style.left = this.charSize.left + 'px';
+    this.elementRef.nativeElement.style.top = this.charSize.top + 'px';
   }
 
   cretaResizingWrapp(){
-    let myDiv = document.createElement('div');
-    myDiv.setAttribute("style", "background-color: blue; width: 20px; height: 20px; position: absolute; right: 0; bottom: 0");
-    this.elementRef.nativeElement.appendChild(myDiv);
+    let myResizer = document.createElement('div');
+    let myMover = document.createElement('div');
+    myResizer.setAttribute("style", "background-color: blue; width: 10px; height: 10px; position: absolute; right: 0; bottom: 0");
+    myMover.setAttribute("style", "background-color: red; width: 10px; height: 10px; position: absolute; right: 0; top: 0");
+    this.elementRef.nativeElement.appendChild(myResizer);
+    this.elementRef.nativeElement.appendChild(myMover);
 
-    return myDiv;
+    return [myResizer, myMover];
   }
 
+  delta_w: number;
+  delta_h: number;
+  resizeFlag: boolean;
+  moveFlag: boolean;
+  
   resizer(block_r){
 
-    let delta_w = 0;
-    let delta_h = 0;
+    this.delta_w = 0;
+    this.delta_h = 0;
 
-    let block = this.elementRef.nativeElement;
-    document.onmouseup = clearXY;
-    block_r.onmousedown = saveWH;
-
-    function saveWH(obj_event) {
-      delta_w = block.clientWidth - obj_event.pageX;
-      delta_h = block.clientHeight - obj_event.pageY;
-
-      document.addEventListener("mousemove", resizeBlock, false);
-      
-      return false; 
+    document.onmouseup = () => {
+      this.resizeFlag = false;
+      this.moveFlag = false;
+      this.clearXY(this.elementRef.nativeElement);
     }
 
-    function clearXY() {
-      document.removeEventListener('mousemove', resizeBlock);
-      localStorage.setItem('charSize', JSON.stringify({ width: block.clientWidth, height: block.clientHeight}));
+    block_r[0].onmousedown = () => {
+      this.resizeFlag = true;
+      this.saveWH(event, this.elementRef.nativeElement);
+    }
+    block_r[1].onmousedown = () => {
+      this.moveFlag = true;
+      this.saveWH(event, this.elementRef.nativeElement);
     }
 
-    function resizeBlock(obj_event) {
-      block.style.width = delta_w + obj_event.pageX + "px";
-      block.style.height = delta_h + obj_event.pageY + "px";
+  }
+
+  saveWH(obj_event, block) {
+    this.delta_w = block.clientWidth - obj_event.pageX;
+    this.delta_h = block.clientHeight - obj_event.pageY;
+
+    window.onmousemove = () => {
+      if(this.resizeFlag == true) this.resizeBlock(event, block);
+      if(this.moveFlag == true) this.moveBlock(event, block);
     }
+    
+    return false; 
+  }
+
+  moveBlock(obj_event, block){
+    block.style.left = obj_event.pageX - block.clientWidth + "px";
+    block.style.top =  obj_event.pageY + "px";
+  }
+
+  resizeBlock(obj_event, block) {
+    block.style.width = this.delta_w + obj_event.pageX + "px";
+    block.style.height = this.delta_h + obj_event.pageY + "px";
+  }
+
+  clearXY(block) {
+    localStorage.setItem('charSize', JSON.stringify({ width: block.clientWidth, height: block.clientHeight, left: block.offsetLeft, top: block.offsetTop}));
   }
 
 }
